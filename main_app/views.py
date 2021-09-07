324d17e99d5df7ad84a2cbe2be13adb14acad132
ziggh28 <haiderzouhrab.1128@gmail.com>
2021-09-07 01:21:07 -0400

    posts_index

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 # Add the following import
 from django.http import HttpResponse
+class post:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, username, category, description, ):
+    self.username = username
+    self.category = category
+    self.description = description
+    
+posts = [
+ post('zigg','yes','helo',),
+ post('zouhrab','yessss','helo',),
+ post('zeek','yessir','helo',),
+]
 
 # Define the home view
 def home(request):
@@ -10,3 +21,7 @@ def home(request):
 
 def post(request):
     return render(request, 'post.html')
+
+def posts_index(request):
+  return render(request, 'posts/index.html', { 'posts': posts })
+
