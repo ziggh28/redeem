825cfff9fc9ca3c8ee7225788c15d7524ae20d22
ziggh28 <haiderzouhrab.1128@gmail.com>
2021-09-09 18:27:35 -0400

    Config deployment

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('login/', views.Login.as_view(), name='login'), 
   path('posts/', views.posts_index, name='posts_index'),
   path('allpost/<int:post_id>/', views.posts_detail, name="post_detail"),
   path('posts/create/', views.postCreate.as_view(), name='post_create'),
   path('posts/<int:pk>/update/', views.postUpdate.as_view(), name='post_update'),
   path('posts/<int:pk>/delete/', views.postDelete.as_view(), name='post_delete'),
   path('accounts/', include('django.contrib.auth.urls')),
-  path('login/', views.Login.as_view(), name='login'), 
   path('accounts/signup/', views.signup, name="signup"),
 ]
